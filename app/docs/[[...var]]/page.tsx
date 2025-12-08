333d9f54f2e653ddb3585841395e49eaa528d0d2
zandergalvez330 <zandergalvez330@gmail.com>
2025-12-08 06:20:14 +0000

    Bad unused

--- app/docs/[[...var]]/page.tsx
+++ app/docs/[[...var]]/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState, useEffect, FC } from "react"
+import React, { useState, useEffect } from "react"
 import { COMPONENTS, DATA } from "@/lib/information"
 import { SelectorLink } from "@/registry/uisydesign/blocks/selector"
 import { LucideIcon, Rocket } from "lucide-react"
